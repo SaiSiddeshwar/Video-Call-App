4b1cb330a556b4695dae0d3c5b0f44c0d1912417
SaiSiddeshwar <sska@deakin.edu.au>
2020-05-21 18:04:27 +1000

    First commit

---
+++ app/src/main/java/com/example/falsedemo/MainActivity.java
@@ -0,0 +1,256 @@
+package com.example.falsedemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import org.minidns.dnsserverlookup.android21.AndroidUsingLinkProperties;
+
+import io.agora.rtc.IRtcEngineEventHandler;
+import io.agora.rtc.RtcEngine;
+import io.agora.rtc.video.VideoCanvas;
+import io.agora.rtc.video.VideoEncoderConfiguration;
+
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int PERMISSION_REQ_ID = 22;
+    private static final String[] REQUESTED_PERMISSIONS = {
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.CAMERA,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    private RtcEngine mRtcEngine;
+    private FrameLayout mLocalContainer;
+    private RelativeLayout mRemoteContainer;
+    private SurfaceView mLocalView;
+    private SurfaceView mRemoteView;
+
+    private ImageView mCallBtn;
+    private ImageView mMuteBtn;
+    private ImageView mSwitchCameraBtn;
+
+    private boolean mCallEnd;
+    private boolean mMuted;
+
+
+    private final IRtcEngineEventHandler mRtcEventHandler = new IRtcEngineEventHandler() {
+
+
+
+        @Override
+        public void onJoinChannelSuccess(String channel, final int uid, int elapsed) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Log.i("sai","Join channel success, uid: " + (uid & 0xFFFFFFFFL));
+                }
+            });
+        }
+
+        @Override
+        public void onUserOffline(final int uid, int reason) {
+            super.onUserOffline(uid, reason);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Log.i("sai","User Offline, uid: " + (uid & 0xFFFFFFFFL));
+                    removeRemoteVideo();
+                }
+            });
+        }
+
+        @Override
+        public void onRemoteVideoStateChanged(final int uid, int width, int height, int elapsed) {
+            super.onRemoteVideoStateChanged(uid, width, height, elapsed);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Log.i("sai","Remove Video Decoded, uid: " + (uid & 0xFFFFFFFFL));
+                    setupRemoteVideo(uid);
+                }
+            });
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AndroidUsingLinkProperties.setup(getApplicationContext());
+//then continue with the usual connection to the xmpp server
+//        XMPPTCPConnection xmppTcpConnection = new XMPPTCPConnection(cc);
+
+        initUI();
+
+        if (checkSelfPermission(REQUESTED_PERMISSIONS[0], PERMISSION_REQ_ID) &&
+                checkSelfPermission(REQUESTED_PERMISSIONS[1], PERMISSION_REQ_ID) &&
+                checkSelfPermission(REQUESTED_PERMISSIONS[2], PERMISSION_REQ_ID)) {
+            initEngineAndJoinChannel();
+//            Log.i("Success", "Full");
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (!mCallEnd) {
+            leaveChannel();
+        }
+        RtcEngine.destroy();
+    }
+
+    private void initUI() {
+        mLocalContainer = findViewById(R.id.local_video_view_container);
+        mRemoteContainer = findViewById(R.id.remote_video_view_container);
+
+        mCallBtn = findViewById(R.id.btn_call);
+        mMuteBtn = findViewById(R.id.btn_mute);
+        mSwitchCameraBtn = findViewById(R.id.btn_switch_camera);
+
+    }
+
+    private void initEngineAndJoinChannel() {
+        initializeEngine();
+        setupVideoConfig();
+        setupLocalVideo();
+        joinChannel();
+    }
+
+    private boolean checkSelfPermission(String permission, int requestCode) {
+        if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, REQUESTED_PERMISSIONS, requestCode);
+            return false;
+        }
+
+        return true;
+    }
+
+    private void initializeEngine() {
+        try {
+            mRtcEngine = RtcEngine.create(getBaseContext(), getString(R.string.agora_app_id), mRtcEventHandler);
+        } catch (Exception e) {
+            Log.e(TAG, Log.getStackTraceString(e));
+            throw new RuntimeException("NEED TO check rtc sdk init fatal error\n" + Log.getStackTraceString(e));
+        }
+    }
+    private void setupVideoConfig() {
+          mRtcEngine.enableVideo();
+
+        mRtcEngine.setVideoEncoderConfiguration(new VideoEncoderConfiguration(
+                VideoEncoderConfiguration.VD_640x360,
+                VideoEncoderConfiguration.FRAME_RATE.FRAME_RATE_FPS_15,
+                VideoEncoderConfiguration.STANDARD_BITRATE,
+                VideoEncoderConfiguration.ORIENTATION_MODE.ORIENTATION_MODE_FIXED_PORTRAIT
+        ));
+    }
+    private void setupLocalVideo() {
+        mRtcEngine.enableVideo();
+        mLocalView = RtcEngine.CreateRendererView(getBaseContext());
+        mLocalView.setZOrderMediaOverlay(true);
+        mLocalContainer.addView(mLocalView);
+         mRtcEngine.setupLocalVideo(new VideoCanvas(mLocalView, VideoCanvas.RENDER_MODE_HIDDEN, 0));
+    }
+
+    private void joinChannel() {
+        String token = null;
+//        if (TextUtils.isEmpty(token) ) {
+//            token = null; // default, no token
+//        }
+        mRtcEngine.joinChannel(null, "demoChannel1", "Extra Optional Data", 0);
+    }
+
+    private void setupRemoteVideo(int uid) {
+        int count = mRemoteContainer.getChildCount();
+        View view = null;
+        for (int i = 0; i < count; i++) {
+            View v = mRemoteContainer.getChildAt(i);
+            if (v.getTag() instanceof Integer && ((int) v.getTag()) == uid) {
+                view = v;
+            }
+        }
+        if (view != null) {
+            return;
+        }
+
+        mRemoteView = RtcEngine.CreateRendererView(getBaseContext());
+        mRemoteContainer.addView(mRemoteView);
+        mRtcEngine.setupRemoteVideo(new VideoCanvas(mRemoteView, VideoCanvas.RENDER_MODE_HIDDEN, uid));
+        mRemoteView.setTag(uid);
+    }
+
+    private void removeRemoteVideo() {
+        if (mRemoteView != null) {
+            mRemoteContainer.removeView(mRemoteView);
+        }
+        mRemoteView = null;
+    }
+
+    public void onLocalAudioMuteClicked(View view) {
+        mMuted = !mMuted;
+        mRtcEngine.muteLocalAudioStream(mMuted);
+        int res = mMuted ? R.drawable.btn_mute : R.drawable.btn_unmute;
+        mMuteBtn.setImageResource(res);
+    }
+    public void onSwitchCameraClicked(View view) {
+        mRtcEngine.switchCamera();
+    }
+
+    public void onCallClicked(View view) {
+        if (mCallEnd) {
+            startCall();
+            mCallEnd = false;
+            mCallBtn.setImageResource(R.drawable.btn_endcall);
+        } else {
+            endCall();
+            mCallEnd = true;
+            mCallBtn.setImageResource(R.drawable.btn_startcall);
+        }
+
+        showButtons(!mCallEnd);
+    }
+
+    private void startCall() {
+        setupLocalVideo();
+        joinChannel();
+    }
+
+    private void endCall() {
+        removeLocalVideo();
+        removeRemoteVideo();
+        leaveChannel();
+    }
+
+    private void removeLocalVideo() {
+        if (mLocalView != null) {
+            mLocalContainer.removeView(mLocalView);
+        }
+        mLocalView = null;
+    }
+
+    private void showButtons(boolean show) {
+        int visibility = show ? View.VISIBLE : View.GONE;
+        mMuteBtn.setVisibility(visibility);
+        mSwitchCameraBtn.setVisibility(visibility);
+    }
+
+    private void leaveChannel() {
+        mRtcEngine.leaveChannel();
+    }
+
+
+
+}
